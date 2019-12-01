21ec40589f0dc7e48cf94bad95b671a88f3c39f4
Jing You <ken32293355@gmail.com>
2019-12-01 23:25:09 +0800

    update testback

--- testback/strategy1.py
+++ testback/strategy1.py
@@ -3,48 +3,68 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# S1  假如ask1預測到上漲機率超過BIAS，直接價外買進，bid1漲一點或是有其他人價外買進或是1秒左右平倉。
+# 假如ask1預測到上漲機率超過BIAS，直接價外買進，n個I080內停損或平倉。
 
 class Strategy1(Testback):
-    def __init__(self, data, buy_bias):
+    def __init__(self, data, buy_bias, stop_steps):
         super(Strategy1, self).__init__(data)
         self.buy_bias = buy_bias
+        self.is_long = True
+        self.stop_steps = stop_steps
     def can_buy(self):
-        if self.possessing == 0 and self._row['nextask1p_label_pred_i'] >= self.buy_bias:
+        if self.possessing == 0 and self.row['nextask1p_label_pred_i'] >= self.buy_bias:
             return True
         else:
             return False
+
     def can_sell(self):
-        if self.possessing == 1 and self._row['nextask1p_label_pred_i']:
-            return True
-        else:
-            return False
+        return (self.possessing_time > self.stop_steps or self.can_make_profit()) and self.possessing > 0
+
     def buy(self):
         self.possessing += 1
         self.possessing_time = 0
-        self.num_exchange += 1
-        self.cost = self._row['']
+        self.cost = self.lastest_ask1p
+
     def sell(self):
+        self.possessing -= 1
+        unrealized_profit = self.unrealized_profit()
+        # print("ask1p", self.lastest_ask1p, "bid1p", self.lastest_bid1p, "cost", self.cost, "unrealized_profit", unrealized_profit)
+        if unrealized_profit > 0:
+            self.earnings.append(unrealized_profit)
+        else:
+            self.losings.append(unrealized_profit)
+    
+    def can_order(self):
+        return False
+
+    def order(self):
         pass
 
+    def can_cancel_order(self):
+        return False
+    def cancel_order(self):
+        pass
 
 if __name__ == "__main__":
 
 
     BIAS  = 0.4
     data = pd.read_csv("./data/0828_tick5_timestep40_askbid1p_withProb.csv")
-    plt.scatter(np.arange(len(data)), data.loc[:, 'nextask1p_label_pred_i'], label = 'ask1 increase')
-    # plt.scatter(np.arange(len(data)), data.loc[:, 'nextask1p_label_pred_d'], label = 'ask1 decrease')
-    plt.scatter(np.arange(len(data)), data.loc[:, 'nextbid1p_label_pred_i'], label = 'bid1 increase')
-    # plt.scatter(np.arange(len(data)), data.loc[:, 'nextbid1p_label_pred_d'], label = 'bid1 decrease')
-    plt.plot(np.arange(len(data)), [BIAS]*len(data))
-    plt.legend()
-    plt.show()
-    print("num of bid1 increase points that prob exceed: %.2f"%(BIAS), np.sum(data.loc[:, 'nextbid1p_label_pred_i'] > BIAS))
-    print("num of ask1 increase points that prob exceed: %.2f"%(BIAS), np.sum(data.loc[:, 'nextask1p_label_pred_i'] > BIAS))
+    # plt.scatter(np.arange(len(data)), data.loc[:, 'nextask1p_label_pred_i'], label = 'ask1 increase')
+    # # plt.scatter(np.arange(len(data)), data.loc[:, 'nextask1p_label_pred_d'], label = 'ask1 decrease')
+    # plt.scatter(np.arange(len(data)), data.loc[:, 'nextbid1p_label_pred_i'], label = 'bid1 increase')
+    # # plt.scatter(np.arange(len(data)), data.loc[:, 'nextbid1p_label_pred_d'], label = 'bid1 decrease')
+    # plt.plot(np.arange(len(data)), [BIAS]*len(data))
+    # plt.legend()
+    # plt.show()
+    # print("num of bid1 increase points that prob exceed: %.2f"%(BIAS), np.sum(data.loc[:, 'nextbid1p_label_pred_i'] > BIAS))
+    # print("num of ask1 increase points that prob exceed: %.2f"%(BIAS), np.sum(data.loc[:, 'nextask1p_label_pred_i'] > BIAS))
 
-    # S1 = Strategy1(
-    #     data=data,
-    #     buy_bias=0.7)
 
-    # S1.run()
+    for stop_steps in [5, 10, 15, 20, 25]:
+        print("stop_steps:  ", stop_steps)
+        S1 = Strategy1(
+            data=data,
+            buy_bias=BIAS,
+            stop_steps=stop_steps)
+        S1.run()
--- testback/testback.py
+++ testback/testback.py
@@ -3,47 +3,142 @@ import numpy as np
 # from abc import ABC
 import abc
 from tqdm import tqdm
-"""
-S1  假如ask1預測到上漲機率超過BIAS，直接價外買進，bid1漲一點或是有其他人價外買進或是1秒左右平倉。
-S2  假如ask1預測到下跌機率超過BIAS，高掛空單，看有沒有機會下到空單。收到I080 or 此價位退至ask2則抽單。
-S3  假如bid1預測到下跌機率超過BIAS，直接價內賣出，跌一點或1秒左右平倉。
-S4  假如bid1預測到上漲機率超過BIAS，高掛多單，看有沒有機會下到多單。收到I080 or 此價位退至bid2則抽單。
-S5  假如ask1且bid1預測到上漲機率超過BIAS，直接價外買進，bid1漲一點或是有其他人價外買進或是1秒左右平倉。
-S6  假如ask1且bid1預測到下跌機率超過BIAS，直接價內賣出，ask1跌一點或是有其他人價外買進或是1秒左右平倉。
-S7  假如ask1預測到上漲機率超過BIAS，低掛多單，bid1漲一點或是有其他人價外買進或是1秒左右平倉。
-S8  假如bid1預測到下跌機率超過BIAS，高掛空單，bid1漲一點或是有其他人價外買進或是1秒左右平倉。
 
 """
+假如ask1預測到上漲機率超過BIAS，直接價外買進，n個I080內停損或平倉。
+假如ask1預測到上漲機率超過BIAS，低掛多單，看n個I080內有沒有機會下到空單，成交後，n個I080內停損或平倉。
+假如ask1預測到下跌機率超過BIAS，高掛空單，看n個I080內有沒有機會下到空單，成交後，n個I080內停損或平倉。
+假如bid1預測到下跌機率超過BIAS，直接價內賣出，n個I080內停損或平倉。
+假如bid1預測到下跌機率超過BIAS，高掛空單，看n個I080內有沒有機會下到空單，成交後，n個I080內停損或平倉。
+假如bid1預測到上漲機率超過BIAS，低掛多單，看n個I080內有沒有機會下到多單，成交後。n個I080內停損或平倉。
+假如ask1且bid1預測到上漲機率超過BIAS，直接價外買進，bid1漲一點或是有其他人價外買進或是1秒左右平倉。
+假如ask1且bid1預測到下跌機率超過BIAS，直接價內賣出，ask1跌一點或是有其他人價外買進或是1秒左右平倉。
+"""
+
 
 class Testback(abc.ABC):
     def __init__(self, data):
-        self._data = data
-        self.earning = 0
-        self.losing = 0
+        self.data = data
+        self.earnings = []
+        self.losings = []
         self.cost = 0
         self.possessing = 0
         self.possessing_time = 0
-        self.num_exchange = 0
-        self._row = None
+        self.order_price = 0
+        self.has_ordered = False
+        self.is_long = False
+        self.row = None
+        self.lastest_ask1p = self.data.loc[0, 'DayHighPrice']
+        self.lastest_bid1p = self.data.loc[0, 'DayLowPrice']
+        self.lastest_price = 0
+
+    @abc.abstractmethod
+    def can_order(self):
+        pass
+
+    @abc.abstractmethod
+    def order(self):
+        pass
+
+    @abc.abstractmethod
+    def can_cancel_order(self):
+        pass
+
+    @abc.abstractmethod
+    def cancel_order(self):
+        pass
+
     @abc.abstractmethod
     def can_buy(self):
         pass
+
     @abc.abstractmethod
     def can_sell(self):
         pass
-    def profit_factor(self):
-        pass
+
     @abc.abstractmethod
     def buy(self):
         pass
+
     @abc.abstractmethod
     def sell(self):
         pass
 
+    def can_make_profit(self):
+        return self.unrealized_profit() > 0
+
+    def unrealized_profit(self):
+        if self.is_long == True:
+            return self.lastest_price - self.cost
+        else:
+            return - self.lastest_price + self.cost
+
+    def _update(self):
+        if self.possessing > 0:
+            self.possessing_time += 1
+
     def run(self):
-        for i in tqdm(range(len(self._data))):
-            self._row = self._data.loc[i]
-            if self.can_buy():
+        for i in range(len(self.data)):
+            self.row = self.data.loc[i]
+            if self.row['is_I080'] == True:
+                self.lastest_ask1p = self.row['ask1p']
+                self.lastest_bid1p = self.row['bid1p']
+            elif self.row['is_I020'] == True:
+                self.lastest_price = self.row['lastPrice']
+            # if i < 50:
+            #     pass
+            # else:
+            #     self.show_result()
+            #     exit()
+
+            if self.can_order():
+                self.order()
+            elif self.can_cancel_order():
+                self.cancel_order()
+            elif self.can_buy():
+                # print("# %d"%(i), "can buy")
                 self.buy()
-            if self.can_sell():
+            elif self.can_sell():
+                # print("# %d"%(i), "can sell")
+                # print("before possesing", self.possessing,"possesing_time", self.possessing_time)
                 self.sell()
+                # print("after possesing, possesing_time", self.possessing, self.possessing_time)
+
+            self._update()
+            # if i > 200:
+            #     self.show_result()
+
+            #     exit()
+
+        self.show_result()
+
+    def profit_factor(self):
+        if len(self.losings) == 0:
+            return np.sum(self.earnings)
+        else:
+            return abs(sum(self.earnings) / sum(self.losings))
+
+    def expected_profit(self):
+        return (sum(self.earnings) + sum(self.losings)) / (len(self.earnings) + len(self.losings))
+
+    def num_exchange(self):
+        return len(self.earnings) + len(self.losings)
+
+    def winrate(self):
+        if len(self.earnings) + len(self.losings) == 0:
+            return 0
+        else:
+            return len(self.earnings) / (len(self.earnings) + len(self.losings))
+
+    def show_result(self):
+        print("""\
+
+num_exchange:"      %d
+profit_factor:      %.3f
+expected_profit:    %.3f
+winrate:            %.3f
+
+"""%(self.num_exchange(), self.profit_factor(), self.expected_profit(), self.winrate()))
+        # print("earning", self.earnings)
+        # print("losing", self.losings)
+
